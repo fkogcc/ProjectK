931397d1bb24f1441acdd98d8875f6018aac1993
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-04-20 18:42:22 +0900

    金・二苦労のファイル追加

---
+++ Project_K/Object/Player/Kinnikurou/KinnnikuIdle.cpp
@@ -0,0 +1,21 @@
+#include "KinnnikuIdle.h"
+
+KinnnikuIdle::KinnnikuIdle()
+{
+}
+
+KinnnikuIdle::~KinnnikuIdle()
+{
+}
+
+void KinnnikuIdle::Init()
+{
+}
+
+void KinnnikuIdle::End()
+{
+}
+
+void KinnnikuIdle::Update()
+{
+}
---
+++ Project_K/Object/Player/Kinnikurou/KinnnikuIdle.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../Base/MoveTypeBase.h"
+
+class KinnnikuIdle : public MoveTypeBase
+{
+public:
+	KinnnikuIdle();
+	virtual ~KinnnikuIdle();
+
+	void Init();// ‰Šú‰»
+	void End();// I—¹
+	void Update();// XV
+
+private:
+};
+
